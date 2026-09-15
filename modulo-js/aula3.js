3b8f5aaa845226062ca628067c3bf394abade5be
Alexandre <alexandrehister6@gmail.com>
2026-09-15 20:31:47 -0300

    aula 3

---
+++ modulo-js/aula3.js
@@ -0,0 +1,30 @@
+/* let contador = 1;
+
+while (contador <= 100) {
+    console.log("Contador", contador);
+    contador += 1;
+}
+
+//contador = contador + 2;
+//contador +=1;
+//contador --;
+
+console.log("Decolar");
+
+const numero = 57;
+
+if (numero % 2 === 0) {
+    console.log("par", contador);
+}
+else if (numero % 2 === 1) {
+    console.log("impar", contador);
+} */
+
+let contador = 0;
+
+while (contador <= 100) {
+    if (contador % 2 === 0) {
+        console.log("par:", contador)
+    }
+    contador++;
+}
